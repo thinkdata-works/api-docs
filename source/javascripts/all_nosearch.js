d634f77ec5781226e18ea7292429a73bf4949c30
Tim Lysecki <tim@thinkdataworks.com>
2018-11-19 14:09:01 -0500

    feature/3: moves jquery to layout.erb

--- source/javascripts/all_nosearch.js
+++ source/javascripts/all_nosearch.js
@@ -15,36 +15,3 @@ window.onpopstate = function() {
   activateLanguage(getLanguageFromQueryString());
 };
 
-$(document).ready(() => {
-
-  $(".see-json").parent().css("margin-bottom", 0)
-
-  $(".see-json").addClass("selected-format")
-  $(".see-csv").addClass("unselected-format")
-  $(".see-geojson").addClass("unselected-format")
-  
-  $(".response-json").next().addClass("show-codeblock")
-  $(".response-csv").next().addClass("hide-codeblock")
-  $(".response-geojson").next().addClass("hide-codeblock")
-
-  $(".see-json").click( () => {
-    $(".see-json").removeClass("unselected-format").addClass("selected-format")
-    $(".see-csv, .see-geojson").removeClass("selected-format").addClass("unselected-format")
-    $(".response-json").next().removeClass("hide-codeblock").addClass("show-codeblock")
-    $(".response-csv, .response-geojson").next().removeClass("show-codeblock").addClass("hide-codeblock")
-  });
-  
-  $(".see-csv").click( () => {
-    $(".see-csv").removeClass("unselected-format").addClass("selected-format")
-    $(".see-json, .see-geojson").removeClass("selected-format").addClass("unselected-format")
-    $(".response-csv").next().removeClass("hide-codeblock").addClass("show-codeblock")
-    $(".response-json, .response-geojson").next().removeClass("show-codeblock").addClass("hide-codeblock")
-  })
-  
-  $(".see-geojson").click( () => {
-    $(".see-geojson").removeClass("unselected-format").addClass("selected-format")
-    $(".see-json, .see-csv").removeClass("selected-format").addClass("unselected-format")
-    $(".response-geojson").next().removeClass("hide-codeblock").addClass("show-codeblock")
-    $(".response-csv, .response-json").next().removeClass("show-codeblock").addClass("hide-codeblock")
-  })
-});
